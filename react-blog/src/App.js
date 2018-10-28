94d96fc16213afb071901af11104534c2374e020
yanan lee <liyananhappy@sina.cn>
2018-10-28 20:57:17 +0800

    update

---
+++ react-blog/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import '../src/css/index.css'
+// import { Wrapper } from './style.js'
+import Header from '../src/common/header'
+// import styled from 'styled-components'
+class App extends Component {
+  render () {
+    return (
+      <Header />
+    )
+  }
+}
+
+export default App
